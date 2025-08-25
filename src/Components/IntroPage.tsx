cd4c947e72eae61e34b7b088be770ef72bfe2f0c
Cassio-Sales <cassiososales@gmail.com>
2025-08-25 12:52:34 +0100

    feat: add responsive intro screen with background image

--- src/Components/IntroPage.tsx
+++ src/Components/IntroPage.tsx
@@ -1,11 +1,18 @@
-import Link from 'next/link';
+import Image from 'next/image'
 
 const IntroPage = () => {
   return (
-    <div>
-      IntroPage
+    <div className="relative w-full min-h-[100dvh] overflow-hidden bg-black flex items-center justify-center">
+      <Image
+        src="/assets/images/intro.png"
+        alt="Tela inicial Bíblia Kids"
+        fill
+        priority
+        sizes="100vw"
+        className="object-contain"
+      />
     </div>
   )
 }
 
-export { IntroPage } 
+export { IntroPage }
