38581a8f6c4b914ab36dc03269308673506ac696
Cassio-Sales <cassiososales@gmail.com>
2025-08-25 13:00:59 +0100

    refactor: object-cover

--- src/Components/IntroPage.tsx
+++ src/Components/IntroPage.tsx
@@ -2,14 +2,14 @@ import Image from 'next/image'
 
 const IntroPage = () => {
   return (
-    <div className="relative w-full min-h-[100dvh] overflow-hidden bg-black flex items-center justify-center">
+    <div className="relative w-full min-h-[100dvh] overflow-hidden bg-black">
       <Image
         src="/assets/images/intro.png"
         alt="Tela inicial Bíblia Kids"
         fill
         priority
         sizes="100vw"
-        className="object-contain"
+        className="object-cover"
       />
     </div>
   )
