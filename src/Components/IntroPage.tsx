91a03f5a698cd34c88cb4516d3f3e2414a1d3175
Cassio-Sales <cassiososales@gmail.com>
2025-09-13 14:40:12 +0100

    feat: add timeout redirect to home, fix navigation back to intro using router.replace and popstate listener, remove unused button

--- src/Components/IntroPage.tsx
+++ src/Components/IntroPage.tsx
@@ -2,6 +2,7 @@
 
 import { Baloo_2 } from 'next/font/google'
 import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
 
 const baloo = Baloo_2({
   subsets: ['latin'],
@@ -15,8 +16,15 @@ const IntroPage = () => {
     router.push('/home')
   }
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push('/home');
+      
+    }, 4000);
+    return () => clearTimeout(timer);
+  },[]);
+
   return (
-    
     <div className="relative w-full min-h-[100dvh] overflow-hidden bg-white">
       {/* Vídeo de fundo */}
       <video
@@ -28,15 +36,14 @@ const IntroPage = () => {
         className="absolute top-0 left-0 w-full h-full object-cover"
       />
 
-      {/* Botão por cima do vídeo */}
-      <div className="absolute bottom-9 w-full flex justify-center">
+      {/*<div className="absolute bottom-9 w-full flex justify-center">
         <button
           onClick={handleStart}
           className={`${baloo.className} bg-[#40A099] text-white px-12 py-2 rounded-full shadow-lg hover:bg-amber-600 transition`}
         >
           <span className="text-3xl font-bold tracking-wide">ENTRAR</span>
         </button>
-      </div>
+      </div>*/}
     </div>
   )
 }
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,27 @@
+'use client'
+import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
+
 import { Home } from './Home'
 
+// prevents the user from navigating back to intro page
 export default function Page() {
+  const router = useRouter()
+
+  useEffect(() => {
+    router.replace('/home') // garante que sempre esteja na home
+
+    const handlePopState = (): void => {
+      router.replace('/home')
+    }
+
+    window.addEventListener('popstate', handlePopState)
+
+    return () => {
+      window.removeEventListener('popstate', handlePopState)
+    }
+  }, [router])
+
   return (
     <div className="min-h-screen bg-white">
       <Home />
