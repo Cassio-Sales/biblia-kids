fde04ed8c5ecd3c2aa99c0264b3eebd0cdff45d7
Cassio-Sales <cassiososales@gmail.com>
2025-09-18 15:45:43 +0100

    fix: update BentoGrid to display 2 columns on mobile

--- src/app/home/Home.tsx
+++ src/app/home/Home.tsx
@@ -3,7 +3,7 @@ import Hero from "../../Components/Hero"
 
 const Home = () => {
   return (
-    <div className="w-full min-h-[100dvh] bg-white flex flex-col items-center">
+    <div className="w-full min-h-[100dvh] max-w-md mx-auto flex flex-col items-center">
       <Hero />
       <CardsSection />
     </div>
