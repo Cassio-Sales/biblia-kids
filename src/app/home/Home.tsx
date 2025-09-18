423df1e859d52ecb607a7579de07ed593c75b310
Cassio-Sales <cassiososales@gmail.com>
2025-09-18 15:39:10 +0100

    fix: adjust Home layout for mobile-first, stacking Hero above CardsSection

--- src/app/home/Home.tsx
+++ src/app/home/Home.tsx
@@ -3,7 +3,7 @@ import Hero from "../../Components/Hero"
 
 const Home = () => {
   return (
-    <div className="w-full min-h-[100dvh] bg-white flex items-center justify-center">
+    <div className="w-full min-h-[100dvh] bg-white flex flex-col items-center">
       <Hero />
       <CardsSection />
     </div>
