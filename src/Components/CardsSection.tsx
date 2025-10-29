a9f213d92a22c26c40187a734920c94a6f80eec4
Cassio-Sales <cassiososales@gmail.com>
2025-10-29 15:47:28 +0000

    feat: add unique IDs to cards and wrap BentoGridItem with dynamic Next.js Link

--- src/Components/CardsSection.tsx
+++ src/Components/CardsSection.tsx
@@ -1,19 +1,25 @@
 'use client'
 
+import Link from 'next/link'
 import { BentoGrid, BentoGridItem } from './ui/BentoGrid'
-import { cards } from '../data/cards' 
+import { cards } from '../data/cards'
 
 const CardsSection = () => {
   return (
     <BentoGrid>
-      {cards.map((card, index) => (
-        <BentoGridItem
-          key={index}
-          title={card.title}
-          description={card.description}
-          header={card.header}
-          icon={card.icon}
-        />
+      {cards.map(card => (
+        <Link
+          key={card.id}
+          href={`/${card.id}`}
+          className="transition-transform hover:scale-[1.02] duration-200"
+        >
+          <BentoGridItem
+            title={card.title}
+            description={card.description}
+            header={card.header}
+            icon={card.icon}
+          />
+        </Link>
       ))}
     </BentoGrid>
   )
